aa4554cea0fd0d8acc236b358fcf0685fcdde4fb
liuhuihao <1061036802@qq.com>
2018-10-10 13:47:50 +0800

    feat: add contrast table

--- src/renderer/pages/contrastTable/index.vue
+++ src/renderer/pages/contrastTable/index.vue
@@ -2,25 +2,22 @@
     <div>
         <div class="contrast-table">
             <div class="search-container">
-                <el-input placeholder="请输入搜索内容" v-model="searchInput" size="mini">
-                    <el-button slot="append" icon="el-icon-search"></el-button>
-                </el-input>
+                <el-input :placeholder="$t('searchPlaceholder')" v-model="searchInput" size="mini"
+                          suffix-icon="el-icon-search"></el-input>
             </div>
             <el-tabs v-model="activeName">
                 <el-tab-pane :label="$t('httpContentType')" name="httpContentType">
-                    <el-table :data="tableData" size="mini" style="width: 100%">
-                        <el-table-column prop="ext" label="文件扩展名"></el-table-column>
+                    <el-table :data="contentTypeData" size="mini" style="width: 100%">
+                        <el-table-column prop="ext" :label="$t('ext')"></el-table-column>
                         <el-table-column prop="type" label="Content-Type(Mime-Type)"></el-table-column>
                     </el-table>
                 </el-tab-pane>
-                <el-tab-pane :label="$t('htmleEscape')" name="htmleEscape">
-                    2
-                </el-tab-pane>
-                <el-tab-pane :label="$t('httpStatus')" name="httpStatus">
-                    3
-                </el-tab-pane>
-                <el-tab-pane :label="$t('keyCode')" name="keyCode">
-                    4
+                <el-tab-pane :label="$t('htmleEscape')" name="htmlEscape">
+                    <el-table :data="htmlEscapeData" size="mini" style="width: 100%">
+                        <el-table-column prop="char" :label="$t('char')"></el-table-column>
+                        <el-table-column prop="decimal" :label="$t('decimal')"></el-table-column>
+                        <el-table-column prop="escape" :label="$t('escape')"></el-table-column>
+                    </el-table>
                 </el-tab-pane>
             </el-tabs>
         </div>
@@ -32,28 +29,57 @@
     "en_US": {
     "httpContentType":"HTTP Content-type",
     "htmleEscape":"HTML escape characters",
-    "httpStatus":"HTTP status codes",
-    "keyCode":"Keyboard key code"
+    "searchPlaceholder":"key words",
+    "ext":"file extension",
+    "char":"char",
+    "decimal":"decimal",
+    "escape":"escape"
     },
-
     "zh_CN": {
     "httpContentType":"HTTP Content-type",
     "htmleEscape":"HTML 转义字符",
-    "httpStatus":"HTTP 状态码",
-    "keyCode":"键盘按键码"
+    "searchPlaceholder":"请输入搜索内容",
+    "ext":"文件扩展名",
+    "char":"字符",
+    "decimal":"十进制",
+    "escape":"转义字符"
     }}
 </i18n>
 
 <script>
-    import contentType from './contentType.json'
+    import contentType from './contentType.json';
+    import htmlEscape from './htmlEscape.json'
 
     export default {
         name: 'hostEdit',
         data() {
             return {
                 activeName: 'httpContentType',
-                tableData: contentType,
+                contentTypeData: contentType,
+                htmlEscapeData: htmlEscape,
+                htmlEscapeBottom: [],
                 searchInput: ''
+            }
+        },
+        watch: {
+            searchInput(newText, oldText) {
+                const contentTypeData = [];
+                contentType.forEach(({ext, type}) => {
+                    if (ext.indexOf(newText) != -1 || type.indexOf(newText) != -1) {
+                        contentTypeData.push({ext, type});
+                    }
+                });
+                const htmlEscapeData = [];
+                htmlEscape.forEach(({char, decimal, escape}) => {
+                    if (char.indexOf(newText) != -1 || decimal.indexOf(newText) != -1 || escape.indexOf(newText) != -1) {
+                        htmlEscapeData.push({char, decimal, escape});
+                    }
+                });
+                this.contentTypeData = contentTypeData;
+                this.htmlEscapeData = htmlEscapeData;
+            },
+            activeName() {
+                this.searchInput = "";
             }
         },
         methods: {},
